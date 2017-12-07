8239eba158f8f00f9645bf87075c35e412c4dd4b
smeetavfernandes <34331581+smeetavfernandes@users.noreply.github.com>
2017-12-07 12:12:17 +0530

    checking

---
+++ utils.js
@@ -0,0 +1,16 @@
+﻿function makeid(len) {
+    if (!len) { len = 5; }
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for (var i = 0; i < len; i++)
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
+
+//for the most part this is not overriding Object
+//so it's good : https://stackoverflow.com/q/14034180/368070
+String.prototype.replaceAll = function (search, replacement) {
+    var target = this;
+    return target.split(search).join(replacement);
+};
